6d4da308378cd485eb0b4e41282f1bbed431d4c4
jcopacetic <jonathansumner90@gmail.com>
2023-12-09 19:21:57 -0600

    Add code for angular-16.2.0 integrate-header-component.
    [Url: razroo.com/community/angular-16.2.0/header/integrate-header-component]

--- apps/angular-test/src/app/app.module.ts
+++ apps/angular-test/src/app/app.module.ts
@@ -10,10 +10,11 @@ import { GlobalSidenavModule } from "@angular-test/common/ui";
 import { MatListModule } from "@angular/material/list";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
+import { GlobalHeaderComponentModule } from "@angular-test/common/ui";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
