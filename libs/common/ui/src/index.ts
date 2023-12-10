55da3541750a4728bdbce58a993a59e202281d36
jcopacetic <jonathansumner90@gmail.com>
2023-12-09 19:20:15 -0600

    Add code for angular-16.2.0 create-common-ui-lib.
    [Url: razroo.com/community/angular-16.2.0/create-common-ui-lib/create-common-ui-lib]

--- libs/common/ui/src/index.ts
+++ libs/common/ui/src/index.ts
@@ -1,5 +1,1 @@
 export * from './lib/ui.module';
-export * from "./lib/layout/layout.component";
-export * from "./lib/global-header/global-header.component";
-export * from "./lib/global-footer/global-footer.component";
-export * from "./lib/global-sidenav/global-sidenav.component";
