b660b59bbf1c35001b327654b8115887799cc059
jcopacetic <jonathansumner90@gmail.com>
2023-12-09 19:21:29 -0600

    Add code for angular-16.2.0 add-homepage-lib.
    [Url: razroo.com/community/angular-16.2.0/add-homepage/add-homepage-lib]

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from "./home/home.component";
-import { HomeRoutingModule } from "./routing/home-routing.module";
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
-  exports: [HomeComponent],
-  declarations: [HomeComponent]
+  imports: [CommonModule],
 })
-export class HomeModule { }
+export class HomeModule {}
